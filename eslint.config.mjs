0b032600d5dcf814e40d743247425b33b7983caf
Aleksei Makonovicky <alexey.makonovicky@altcraft.com>
2025-04-02 17:33:09 +0300

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,50 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-  rules: {
-        '@typescript-eslint/no-unused-vars': 'warn',
-        '@typescript-eslint/no-explicit-any': 'warn',
-        'react/jsx-uses-react': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-        'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
-        'prefer-const': 'error',
-        'no-var': 'error',
-        '@typescript-eslint/no-for-in-array': 'error',
-        '@typescript-eslint/naming-convention': [
-            'warn',
-            {
-                selector: 'variable',
-                format: ['camelCase', 'PascalCase'],
-                leadingUnderscore: 'allow',
-                trailingUnderscore: 'allow',
-            },
-        ],
-        '@typescript-eslint/no-loop-func': 'error',
-        'max-len': [
-            'warn',
-            {
-                ignoreStrings: true,
-                ignoreRegExpLiterals: true,
-                ignoreTemplateLiterals: true,
-                code: 120,
-            },
-        ],
-        indent: ['warn', 4],
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            'react/jsx-uses-react': 'off',
+            'react/react-in-jsx-scope': 'off',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
+            'prefer-const': 'error',
+            'no-var': 'error',
+            '@typescript-eslint/no-for-in-array': 'error',
+            '@typescript-eslint/naming-convention': [
+                'warn',
+                {
+                    selector: 'variable',
+                    format: ['camelCase', 'PascalCase'],
+                    leadingUnderscore: 'allow',
+                    trailingUnderscore: 'allow',
+                },
+            ],
+            '@typescript-eslint/no-loop-func': 'error',
+            'max-len': [
+                'warn',
+                {
+                    ignoreStrings: true,
+                    ignoreRegExpLiterals: true,
+                    ignoreTemplateLiterals: true,
+                    code: 120,
+                },
+            ],
+            indent: ['warn', 4],
+        },
     },
-  }
 ];
 
 export default eslintConfig;
